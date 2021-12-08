29a0dfa947f3958d7f758f563f09d27b0998718f
Pratima <pratimas@zimetrics.com>
2021-12-08 10:35:27 +0530

    first commit

---
+++ src/test/java/PratimaS/RestAPITest.java
@@ -0,0 +1,22 @@
+package PratimaS;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void browserAutomation() {
+		
+		System.out.println("BrowserAutomation");
+	
+	}
+	
+	@Test
+	public void elementsUI() {
+		
+		System.out.println("ElementsUI");
+	
+	}
+
+}
---
+++ src/test/java/PratimaS/SeleniumTest.java
@@ -0,0 +1,21 @@
+package PratimaS;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void postJira() {
+		
+		System.out.println("postJira");
+	
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		
+		System.out.println("deleteTwitter");
+	
+	}
+
+}
